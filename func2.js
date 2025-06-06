ce98c6a3a02c18ac25671032b49e4691b563cca0
hardika05 <shindehardika05@gmail.com>
2025-06-06 19:17:34 +0530

    Object

--- func2.js
+++ func2.js
@@ -1,7 +1,3 @@
-function greet(name='Guest'){
-    return `Hello,${name}!`;
-}
-console.log(greet());
 function greet2(name,callback){
     console.log("Hi "+name);
     callback();
@@ -9,4 +5,4 @@ function greet2(name,callback){
 function sayBye(){
     console.log("GoodBye!");
 }
-greet("Hardika",sayBye);
+greet2("Hardika",sayBye);
---
+++ object.js
@@ -0,0 +1,13 @@
+const person={
+    name: "John",
+    age: 30,
+    isStudent: false,
+    address:{
+        street: "123 Main St",
+        city: "Exampleville"
+    },
+    hobbies: ["Reading","Gardening","Cooking"],
+    sayHello: function(){
+        console.log("Hello!");
+    }
+};
