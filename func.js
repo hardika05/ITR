2a94960f4935893dfbf0b2456909e1d848bf15b9
hardika05 <shindehardika05@gmail.com>
2025-06-06 18:31:11 +0530

    Callback

--- func.js
+++ func.js
@@ -1,4 +1,24 @@
-const greet= function(name){
-    return 'Hello ${name}';
-};
-console.log(greet('Bob'));
+// const greet= function(name){
+//     return `Hello , ${name}`;
+// };
+// console.log(greet('Bob'));
+function greet1(name='Guest'){
+    return `Hello, ${name}`;
+}
+console.log(greet1());
+// function sum(...numbers){
+//     return numbers.reduce((total,num)=>total + num,0);
+// }
+// console.log(sum(1,2,3));
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,3));
+function greet1(name,callback){
+    console.log("Hi"+name);
+    callback();
+}
+function sayBye(){
+    console.log("GoodBye!");
+}
+greet("Prathamesh",sayBye);
